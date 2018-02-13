c486b9a32ba0f8e44f761d06431410b41ac06a48
cherweg <christian.herweg@gmail.com>
2018-02-13 10:43:09 +0100

    Extend to PUBLIC and PRIVATE ZONES

--- route53-asg-dns.sh
+++ route53-asg-dns.sh
@@ -16,6 +16,11 @@ case $key in
     shift # past argument
     shift # past value
     ;;
+    -z|--zone-type)
+    ZONE_TYPE="$2"
+    shift # past argument
+    shift # past value
+    ;;    
     -t|--name-tag)
     NAME_TAG="$2"
     shift # past argument
@@ -29,8 +34,8 @@ esac
 done
 
 set -- "${POSITIONAL[@]}" # restore positional parameters
-([ -n "$DOMAIN" ]  &&  [ -n "$NAME_TAG" ]) || {
-  echo >&2 "Usage: $0 -d <domain> -t <name-tag> -a <auto-scaling-group>"
+([ -n "$DOMAIN" ]  &&  [ -n "$NAME_TAG" ]) &&  [ -n "$ZONE_TYPE" ]) || {
+  echo >&2 "Usage: $0 -d <domain> -z <zone-type> -t <name-tag> -a <auto-scaling-group>"
   exit 2
 }
 
@@ -85,6 +90,13 @@ if [ -z "$GROUP" ]; then
   GROUP=$(aws autoscaling describe-auto-scaling-groups --region eu-central-1 --query "AutoScalingGroups[?contains(Tags[?Key==\`Name\`].Value, \`${NAME_TAG}\`)].[AutoScalingGroupName]" --output text)
 fi
 gen_json "$GROUP" > /tmp/${GROUP}.json
-ZONE_ID=$(aws route53 list-hosted-zones-by-name --dns-name ${DOMAIN} --region eu-central-1 --query 'HostedZones[*].Id' --output text)
+if [ "$ZONE_TYPE" == "PUBLIC" ]; then 
+  ZONE_ID=$(aws route53 list-hosted-zones-by-name --dns-name ${DOMAIN} --region eu-central-1 --max-items 1 --query 'HostedZones[?Config.PrivateZone == `false` ].Id' --output text)
+elif [ "$ZONE_TYPE" == "PRIVATE" ]; then
+  ZONE_ID=$(aws route53 list-hosted-zones-by-name --dns-name ${DOMAIN} --region eu-central-1 --max-items 1 --query 'HostedZones[?Config.PrivateZone == `true` ].Id' --output text)
+else
+  echo >&2 "ZONE_TYPE must be PUBLIC||PRIVATE"
+  exit 3
+fi
 ZONE_ID=${ZONE_ID##/hostedzone/}
 aws route53 change-resource-record-sets --region eu-central-1 --hosted-zone-id $ZONE_ID --change-batch file:///tmp/${GROUP}.json
